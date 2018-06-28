672ed3a08827ea29fe7c3676bd7b87f5e7b69b02
WeiXiang <wojiaoweixiang@qq.com>
2018-06-28 14:17:43 +0800

    first commit

---
+++ src/main/java/com/neu/controller/GlobalExceptionResolver.java
@@ -0,0 +1,9 @@
+package com.neu.controller;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.handler.HandlerExceptionResolverComposite;
+
+public class GlobalExceptionResolver extends HandlerExceptionResolverComposite {
+
+	
+}
