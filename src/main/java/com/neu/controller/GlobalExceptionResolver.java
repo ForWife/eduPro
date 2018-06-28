47f275b0d97bc66d36ea521b261980953fcdfd96
WeiXiang <wojiaoweixiang@qq.com>
2018-06-28 14:46:04 +0800

    back and front

--- src/main/java/com/neu/controller/GlobalExceptionResolver.java
+++ src/main/java/com/neu/controller/GlobalExceptionResolver.java
@@ -1,9 +1,19 @@
 package com.neu.controller;
 
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.handler.HandlerExceptionResolverComposite;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class GlobalExceptionResolver extends HandlerExceptionResolverComposite {
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+public class GlobalExceptionResolver implements HandlerExceptionResolver{
+
+	@Override
+	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
+			Exception ex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	
 }
